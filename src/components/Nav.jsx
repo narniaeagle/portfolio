cd0fddf541c20784d4d0399ae6cc8d149adb82b8
narniaeagle <ilgar2023@hotmail.com>
2023-02-16 10:00:57 -0500

    template

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+
+
+const Nav = () => {
+    return(
+        <div class="nav-top">
+            <span class="nav-top-items">Projects</span>
+            <span class="nav-top-items">Home</span>
+            <span class="nav-top-items">About Me</span>
+        </div>
+    )
+}
+
+export default Nav

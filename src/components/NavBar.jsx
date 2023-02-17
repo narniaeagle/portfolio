883586b79ef529750c89ed7ca6a2bc4c1798f18c
narniaeagle <ilgar2023@hotmail.com>
2023-02-17 18:21:38 -0500

    minor changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
     return(
         <div className="nav-top">
-            <span className="nav-top-items"><Link to="./Projects">Projects</Link></span>
-            <span className="nav-top-items"><Link to="/">Home</Link></span>
-            <span className="nav-top-items"><Link to="./About"><button>About</button></Link></span>
+            <span className="nav-top-items"><Link to="./Projects"><button className="btn1">Projects</button></Link></span>
+            <span className="nav-top-items"><Link to="/"><button className="btn1">Home</button></Link></span>
+            <span className="nav-top-items"><Link to="./About"><button className="btn1">About</button></Link></span>
 
         </div>
     )

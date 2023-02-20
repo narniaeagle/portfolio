57fb89e19066f45dbba1e4da15f77e0620aa6c40
narniaeagle <ilgar2023@hotmail.com>
2023-02-19 20:23:41 -0500

    some coloring

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,15 @@
 
 const Home = () => {
     return(
-        <h2>Home</h2>
+        <div className="home">
+            <div className="image-container">
+                <img className="pimage"src="https://www.befunky.com/images/prismic/5ddfea42-7377-4bef-9ac4-f3bd407d52ab_landing-photo-to-cartoon-img5.jpeg?auto=avif" alt="photo of me" />
+            </div>
+            <div className="land-text">
+                <h4>As a graduate of computer programming, I am looking for a role where I can grow and learn from experienced team members.
+                    As a having personality of gets what he wants, I will go through the obstacles and be successful in my career goal. </h4>
+            </div>
+        </div>
     )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
     return(
         <div className="nav-top">
-            <span className="nav-top-items"><Link to="./Projects"><button className="btn1">Projects</button></Link></span>
-            <span className="nav-top-items"><Link to="/"><button className="btn1">Home</button></Link></span>
-            <span className="nav-top-items"><Link to="./About"><button className="btn1">About</button></Link></span>
+            <span className="nav-top-items"><Link to="./Projects"><button className="btn1">PROJECTS</button></Link></span>
+            <span className="nav-top-items"><Link to="/"><button className="btn1">HOME</button></Link></span>
+            <span className="nav-top-items"><Link to="./About"><button className="btn1">ABOUT</button></Link></span>
 
         </div>
     )
